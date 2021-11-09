401fcd3c44919d373e344c2b39438fc0f3e0dd31
Fifi <62918632+Fifi-rezaeian@users.noreply.github.com>
2021-11-08 18:15:37 -0800

    Update app.py

--- Audit_log/app.py
+++ Audit_log/app.py
@@ -92,6 +92,8 @@ def get_report_scheduled_order_details(index):
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
+CORS(app.app)
+app.app.config['CORS_HEADERS'] = 'Content-Type'
 app.add_api("openapi.yml",strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
