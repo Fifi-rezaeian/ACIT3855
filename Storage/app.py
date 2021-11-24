afd15484ec2830e96d90129317b55fded0140065
Fifi <62918632+Fifi-rezaeian@users.noreply.github.com>
2021-11-24 10:46:28 -0800

    Update app.py

--- Storage/app.py
+++ Storage/app.py
@@ -90,8 +90,8 @@ def get_report_order_details(start_timestamp, end_timestamp):
     """ Gets new order details readings after the timestamp """
 
     session = DB_SESSION()
-    start_timestamp_datetime = datetime.datetime.strptime(start_timestamp,"%Y-%m-%dT%H:%M:%SZ")
-    end_timestamp_datetime = datetime.datetime.strptime(end_timestamp, "%Y-%m-%dT%H:%M:%SZ")
+    start_timestamp_datetime = datetime.datetime.strptime(start_timestamp,"%Y-%m-%d")
+    end_timestamp_datetime = datetime.datetime.strptime(end_timestamp, "%Y-%m-%d")
 
     readings= session.query(Order).filter(
         and_(Order.date_created >= start_timestamp_datetime, Order.date_created < end_timestamp_datetime)
@@ -113,8 +113,8 @@ def get_report_scheduled_order_details(start_timestamp, end_timestamp):
     """ Gets new order details readings after the timestamp """
 
     session = DB_SESSION()
-    start_timestamp_datetime = datetime.datetime.strptime(start_timestamp,"%Y-%m-%dT%H:%M:%SZ")
-    end_timestamp_datetime = datetime.datetime.strptime(end_timestamp, "%Y-%m-%dT%H:%M:%SZ")
+    start_timestamp_datetime = datetime.datetime.strptime(start_timestamp,"%Y-%m-%d")
+    end_timestamp_datetime = datetime.datetime.strptime(end_timestamp, "%Y-%m-%d")
 
     readings= session.query(ScheduledOrder).filter(
         and_(ScheduledOrder.date_created >= start_timestamp_datetime, ScheduledOrder.date_created < end_timestamp_datetime)
