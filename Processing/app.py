aebad2fcc5afb8c6ae6e6079f0308b385953e06b
Fifi <62918632+Fifi-rezaeian@users.noreply.github.com>
2021-11-30 08:02:23 -0800

    Update app.py

--- Processing/app.py
+++ Processing/app.py
@@ -21,8 +21,6 @@ if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
     app_conf_file = "/config/app_conf.yml"
     log_conf_file = "/config/log_conf.yml"
 else:
-    CORS(app.app)
-    app.app.config['CORS_HEADERS'] = 'Content-Type'
     print("In Dev Environment")
     app_conf_file = "app_conf.yml"
     log_conf_file = "log_conf.yml"
@@ -140,8 +138,9 @@ def init_scheduler():
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-#CORS(app.app)
-#app.app.config['CORS_HEADERS'] = 'Content-Type'
+if "TARGET_ENV" not in os.environ or os.environ["TARGET_ENV"] != 'test':
+    CORS(app.app)
+    app.app.config['CORS_HEADERS']='Content-Type'
 app.add_api("openapi.yaml", base_path="/processing", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
